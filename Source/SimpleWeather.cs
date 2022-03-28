ac3aad13859a86d1613f1db15bc1494195bd888d
Ivan Gechev <ivan_banov@abv.bg>
2022-03-28 17:13:55 +0300

    Removed Controller updated SimpleWeather class

--- Source/SimpleWeather.cs
+++ Source/SimpleWeather.cs
@@ -1,7 +1,36 @@
-﻿namespace SimpleWeather
-{
-    public class SimpleWeather
-    {
+﻿using Microsoft.Extensions.Configuration;
+using System.Web.Http;
 
+namespace SimpleWeather
+{
+    public class SimpleWeather : ApiController
+    {
+        public static string _baseUrl = $"https://api.openweathermap.org/data/2.5/weather?q=";
+        public new IConfiguration Configuration { get; private set; }
+
+        public SimpleWeather() : base()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .Build();
+        }
+
+        public async Task<CurrentWeather?> GetWeatherResponse(string cityName = "Lovech", string units = "metric")
+        {
+            var baseAddress = new Uri(_baseUrl + cityName + "&appid=" + Configuration["openWeatherApiKey"] + $"&units={units}");
+
+            var client = new HttpClient()
+            {
+                BaseAddress = baseAddress
+            };
+
+            HttpResponseMessage weatherResponse = await client.GetAsync(baseAddress);
+
+            var jsonContent = await weatherResponse.Content.ReadAsStringAsync();
+
+            CurrentWeather? currentWeather = new CurrentWeather(jsonContent);
+
+            return currentWeather;
+        }
     }
 }

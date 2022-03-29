28d4371cc2ab9f198279d0d29c7d50bd78295c36
Ivan Gechev <ivan_banov@abv.bg>
2022-03-29 17:37:27 +0300

    Added logic to extract sunset/sunrise time in seperate property

--- Source/Classes/Sys.cs
+++ Source/Classes/Sys.cs
@@ -14,6 +14,8 @@ namespace SimpleWeather
                 // TODO: Extract only time
                 Sunrise = UnixToDateTime(double.Parse(data.SelectToken("sunrise").ToString()));
                 Sunset = UnixToDateTime(double.Parse(data.SelectToken("sunset").ToString()));
+                SunriseTime = DateTimeToSimpleTime(Sunrise);
+                SunsetTime = DateTimeToSimpleTime(Sunset);
             }
         }
 
@@ -21,18 +23,31 @@ namespace SimpleWeather
         public int ID { get; }
         public string Country { get; }
         /// <summary>
-        /// Sunrise time converted to your local time
+        /// A DateTime object representing sunrise time converted to your local time
         /// </summary>
         public DateTime Sunrise { get; }
         /// <summary>
-        /// Sunset time converted to your local time
+        /// Time of sunrise in the format HH:mm
+        /// </summary>
+        public string SunriseTime { get; }
+        /// <summary>
+        /// A DateTime object representing sunset time converted to your local time
         /// </summary>
         public DateTime Sunset { get; }
+        /// <summary>
+        /// Time of sunset in the format HH:mm
+        /// </summary>
+        public string SunsetTime { get; }
 
-        public static DateTime UnixToDateTime(double unixTime)
+        private static DateTime UnixToDateTime(double unixTime)
         {
             DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddSeconds(unixTime).ToLocalTime();
+        }
+
+        private static string DateTimeToSimpleTime(DateTime dateTime)
+        {
+            return dateTime.ToString("t");
         }
     }
 }
